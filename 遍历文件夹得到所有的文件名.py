f7a729f6384a0148c2c9f35e15ce3d42d472b60e
TangXiran <37133564+tangxiran@users.noreply.github.com>
2020-10-24 13:29:55 +0800

    Update 遍历文件夹得到所有的文件名.py

--- 遍历文件夹得到所有的文件名.py
+++ 遍历文件夹得到所有的文件名.py
@@ -8,14 +8,19 @@ def getFileName(dirName):
         print(i, j, k)
         fileList.append(k)
     return fileList[0]
-# 特定的文件后缀保存
-def data_select(data_dir):  #  
-    import  glob
-    file_list = list(glob.glob(data_dir + '/*.png')) + list(glob.glob(data_dir + '/*.jpg'))   # get name list of all .png files
-    data = []
-    print(file_list) # 得到文件的路径列表
-    return file_list
 
+# 特定的文件后缀保存，筛选后的特定后缀
+def data_select(data_dir,*args):  #
+    import  glob
+    # *args是要筛选的的后缀名称可以选择多个
+    length = len(args)
+    file_list = []
+    for i in range(length):
+        file_list = file_list + list(glob.glob(data_dir + '/*.'+str(args[i])))
+    # file_list = list(glob.glob(data_dir + '/*.png')) + list(glob.glob(data_dir + '/*.jpg'))   # get name list of all .png files
+    # data = []
+    # print(file_list) # 得到文件的路径列表
+    return file_list
 if __name__ == '__main__':
     dir = '../gray_origin'
     flist_to_writed = 'test.flist'
